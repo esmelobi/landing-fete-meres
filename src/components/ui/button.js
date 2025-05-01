905c55d9f9332d54b4142cfbd84c0cd02a9b4336
esmelobi <obihenri@gmail.com>
2025-05-01 00:00:49 +0000

    Delete src directory

--- src/components/ui/button.js
+++
@@ -1,3 +0,0 @@
-export function Button({ children, onClick, className }) {
-  return <button onClick={onClick} className={`bg-blue-600 text-white px-4 py-2 rounded-xl hover:bg-blue-700 ${className}`}>{children}</button>;
-}
